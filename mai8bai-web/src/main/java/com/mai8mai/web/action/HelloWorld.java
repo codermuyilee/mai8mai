c46d8b9a93b5bfc4e3aa66b4470b2864e393739c
muyilee <muyilee@126.com>
2016-01-05 18:21:37 +0800

    初次提交

---
+++ mai8bai-web/src/main/java/com/mai8mai/web/action/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.mai8mai.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.*;
+
+@ParentPackage("struts-default")
+// 父包
+@Namespace("/helloworld")
+@Results( { @Result(name = "success", location = "/msg.jsp"),
+        @Result(name = "error", location = "/error.jsp") })
+@ExceptionMappings( { @ExceptionMapping(exception = "java.lange.RuntimeException", result = "error") })
+public class HelloWorld extends ActionSupport {
+    public final static String MESSAGE = "Struts2 is up and running ..1.";
+
+    private String message;
+
+
+    /**
+     * @return the message
+     */
+    @Action(value = "hello",results = {@Result(name="success",location = "/HelloWorld.jsp")})
+    public String hello() {
+        message=MESSAGE;
+        return "success";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
