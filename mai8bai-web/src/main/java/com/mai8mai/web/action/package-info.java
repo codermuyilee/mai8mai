a0acea07c4566e9008b771d47d5e4c0d422e989d
muyilee <muyilee@126.com>
2016-01-07 17:06:23 +0800

    引入freemarker，能正常跑起来

--- mai8bai-web/src/main/java/com/mai8mai/web/action/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParentPackage("struts-default")
-
-@Namespace("/helloworld")
-package com.mai8mai.web.action;
-
-import org.apache.struts2.convention.annotation.Namespace;
-import org.apache.struts2.convention.annotation.ParentPackage;
---
+++ mai8mai-web/src/main/java/com/mai8mai/web/action/package-info.java
@@ -0,0 +1,7 @@
+@ParentPackage("struts-default")
+
+@Namespace("/helloworld")
+package com.mai8mai.web.action;
+
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
